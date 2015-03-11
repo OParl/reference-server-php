52a51e3f5f4bc488e55c55ece934b419c4ca343e
Stefan Graupner <stefan.graupner@gmail.com>
2015-03-11 12:07:03 +0100

    Added ?format=json option

--- app/Http/Controllers/API/Controller.php
+++ app/Http/Controllers/API/Controller.php
@@ -1,6 +1,7 @@
 <?php namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller as BaseController;
+use Illuminate\Support\Facades\Input;
 use \Response;
 
 use Illuminate\Http\Request;
@@ -11,6 +12,9 @@ class Controller extends BaseController
 
   protected $statusCode = 200;
 
+  /**
+   * @param Request $request
+   */
   public function __construct(Request $request)
   {
     $this->request = $request;
@@ -25,9 +29,15 @@ class Controller extends BaseController
     return $this;
   }
 
+  /**
+   * @param $data
+   * @param array $headers
+   * @return mixed
+   */
   protected function respond($data, array $headers = [])
   {
-    if ($this->request->wantsJson())
+    if ($this->request->wantsJson()
+    ||  $this->request->input('format') === 'json')
     {
       return Response::json($data, $this->statusCode, $headers);
     } else
@@ -36,7 +46,7 @@ class Controller extends BaseController
 
       // gzip?!
       if (array_has($this->request->getAcceptableContentTypes(), 'gzip')
-        ||  array_has($this->request->getAcceptableContentTypes(), 'compress'))
+      ||  array_has($this->request->getAcceptableContentTypes(), 'compress'))
       {
         $headers['Content-encoding'] = 'gzip';
         $data = gzcompress($data);
