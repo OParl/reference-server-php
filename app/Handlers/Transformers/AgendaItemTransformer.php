da861d6aec3d721997bce3f3446cfb802121c41e
Stefan Graupner <stefan.graupner@gmail.com>
2015-03-27 14:10:17 +0100

    + AgendaItem transformer

--- app/Handlers/Transformers/AgendaItemTransformer.php
+++ app/Handlers/Transformers/AgendaItemTransformer.php
@@ -4,10 +4,34 @@ use Oparl\AgendaItem;
 
 class AgendaItemTransformer extends TransformerAbstract
 {
+  protected $availableIncludes = ['meeting', 'consultation'];
+
   public function transform(AgendaItem $agendaItem)
   {
     return [
-      'id' => $agendaItem->id
+      'id' => route('api.v1.agendaitem.show', $agendaItem->id),
+      'type' => 'http://oparl.org/schema/1.0/AgendaItem',
+      'meeting' => route('api.v1.meeting.show', $agendaItem->meeting_id),
+      'number' => $agendaItem->number,
+      'name'  => $agendaItem->name,
+      'public' => (boolean)$agendaItem->public,
+      'consultation' => ($agendaItem->consultation_id)
+                          ? route('api.v1.consultation.show', $agendaItem->consultation_id)
+                          : null,
+      'result' => $agendaItem->result,
+      'resolution' => $agendaItem->resolution,
+      'created' => $this->formatDate($agendaItem->created_at),
+      'updated' => $this->formatDate($agendaItem->modified_at)
     ];
+  }
+
+  public function includeMeeting(AgendaItem $agendaItem)
+  {
+    return $this->item($agendaItem->meeting, new MeetingTransformer);
+  }
+
+  public function includeConsultation(AgendaItem $agendaItem)
+  {
+    return $this->item($agendaItem->consultation, new ConsultationTransformer);
   }
 }
--- oparl-model/AgendaItem.php
+++ oparl-model/AgendaItem.php
@@ -7,10 +7,24 @@ class AgendaItem extends Model {
 	//
   public function auxiliaryFiles()
   {
-    $this->belongsToMany('OParl\File', 
+    return $this->belongsToMany('OParl\File',
                          'agendaitems_auxiliary_files',
                          'agendaitem_id',
                          'auxiliary_id');
   }
 
+  public function papers()
+  {
+    return $this->hasManyThrough('OParl\Paper', 'OParl\Consultation', 'agenda_item_id', 'paper_id');
+  }
+
+  public function consultation()
+  {
+    return $this->hasOne('OParl\Consultation', 'consultation_id');
+  }
+
+  public function meeting()
+  {
+    return $this->belongsTo('OParl\Meeting', 'meeting_id');
+  }
 }
