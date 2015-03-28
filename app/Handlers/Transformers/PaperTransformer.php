cb7022289fcb06c11e7fc102ef86899a0e0e6dd6
Stefan Graupner <stefan.graupner@gmail.com>
2015-03-28 18:02:52 +0100

    Transforming papers.

--- app/Handlers/Transformers/PaperTransformer.php
+++ app/Handlers/Transformers/PaperTransformer.php
@@ -7,7 +7,23 @@ class PaperTransformer extends TransformerAbstract
   public function transform(Paper $paper)
   {
     return [
-      'id' => $paper->id
+      'id'            => route('api.v1.paper.show', $paper->id),
+      'body'          => route('api.v1.body.show', $paper->body_id),
+      'name'          => $paper->name,
+      'reference'     => $paper->reference,
+      'paperType'     => $paper->paper_type,
+      'relatedPaper'  => $this->collectionRouteList('api.v1.paper.show', $paper->relatedPapers),
+      'mainFile'      => route('api.v1.file.show', $paper->main_file_id),
+      'auxiliaryFile' => $this->collectionRouteList('api.v1.file.show', $paper->auxiliaryFiles),
+      'location'      => route('api.v1.location.show', $paper->location_id),
+      'originator'    => null, // FIXME: This really is BAD SCHEMA DESIGN
+      'consultation'  => $this->collectionRouteList('api.v1.consultation.show', $paper->consultations),
+      'keyword'       => $paper->keyword,
+
+      'underDirectionOf' => route('api.v1.organization.show', $paper->under_direction_of_id),
+
+      'publishedDate' => $this->formatDate('published_date'),
+      'modified'      => $this->formatDate('modified_at'),
     ];
   }
 }
--- oparl-model/Paper.php
+++ oparl-model/Paper.php
@@ -4,6 +4,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Paper extends Model {
 
-	//
+	public function body()
+  {
+    return $this->belongsTo('OParl\Body');
+  }
 
+  public function relatedPapers()
+  {
+    return $this->belongsToMany('OParl\Paper', 'papers_related_papers', 'paper_id', 'related_id');
+  }
+
+  public function auxiliaryFiles()
+  {
+    return $this->belongsToMany('OParl\Paper', 'papers_auxiliary_files', 'paper_id', 'auxiliary_id');
+  }
+
+  public function consultations()
+  {
+    return $this->belongsToMany('OParl\Consultation', 'papers_consultations', 'paper_id', 'consultation_id');
+  }
 }
