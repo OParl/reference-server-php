f1c539e4419712f756cdfdddac1e5d8e00f79fe9
Stefan Graupner <stefan.graupner@gmail.com>
2015-03-29 12:48:45 +0200

    Locations transforming

--- app/Handlers/Transformers/LocationTransformer.php
+++ app/Handlers/Transformers/LocationTransformer.php
@@ -7,7 +7,12 @@ class LocationTransformer extends TransformerAbstract
   public function transform(Location $location)
   {
     return [
-      'id' => $location->id
+      'id'          => route('api.v1.location.show', $location->id),
+      'type'        => 'http://oparl.org/schema/1.0/Location',
+      'description' => $location->description,
+      'address'     => $location->address,
+      'geometry'    => $location->geometry,
+      'keyword'     => $location->keyword
     ];
   }
 }
