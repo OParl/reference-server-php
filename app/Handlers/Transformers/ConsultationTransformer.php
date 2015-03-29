6a0287699531d6e5f9273645882b8a620ee9baae
Stefan Graupner <stefan.graupner@gmail.com>
2015-03-29 12:44:13 +0200

    Consultations transforming

--- app/Handlers/Transformers/ConsultationTransformer.php
+++ app/Handlers/Transformers/ConsultationTransformer.php
@@ -4,10 +4,34 @@ use Oparl\Consultation;
 
 class ConsultationTransformer extends TransformerAbstract
 {
+  protected $availableIncludes = ['paper', 'agendaItem', 'organization'];
+
   public function transform(Consultation $consultation)
   {
     return [
-      'id' => $consultation->id
+      'id'            => route('api.v1.consultation.show', $consultation->id),
+      'type'          => 'http://oparl.org/schema/1.0/Consultation',
+      'paper'         => route('api.v1.paper.show', $consultation->paper_id),
+      'agendaItem'    => route('api.v1.agendaitem.show', $consultation->agenda_item_id),
+      'organization'  => $this->collectionRouteList('api.v1.organization.show', $consultation->organizations),
+      'authoritative' => (boolean)$consultation->authoritative,
+      'role'          => $consultation->role,
+      'keyword'       => $consultation->keyword
     ];
+  }
+
+  public function includePaper(Consultation $consultation)
+  {
+    return $this->item($consultation->paper, new PaperTransformer);
+  }
+
+  public function includeAgendaItem(Consultation $consultation)
+  {
+    return $this->item($consultation->agendaItem, new AgendaItemTransformer);
+  }
+
+  public function includeOrganization(Consultation $consultation)
+  {
+    return $this->collection($consultation->organization, new OrganizationTransformer);
   }
 }
--- oparl-model/Consultation.php
+++ oparl-model/Consultation.php
@@ -4,6 +4,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Consultation extends Model {
 
-	//
+  public function paper()
+  {
+    return $this->hasOne('OParl\Paper');
+  }
+
+  public function agendaItem()
+  {
+    return $this->belongsTo('OParl\AgendaItem', 'agenda_item_id', 'id');
+  }
+
+	public function consultations()
+  {
+    return $this->belongsToMany('OParl\Consultation', 'consultations_organizations', 'consultation_id', 'organization_id');
+  }
 
 }
