e35c29154930326bcc9e6aebbd91c396994a0470
Stefan Graupner <stefan.graupner@gmail.com>
2015-04-10 13:01:41 +0200

    Refactored argument parsing.

--- app/Http/Controllers/API/APIIndexPaginatedTrait.php
+++ app/Http/Controllers/API/APIIndexPaginatedTrait.php
@@ -4,45 +4,76 @@ use \Request;
 
 trait APIIndexPaginatedTrait
 {
+  private function parseWhere($where)
+  {
+    $query = null;
+
+    $clauses = decode_where($where);
+
+    $clauses = array_filter_keys($clauses, function($clause) {
+      return class_method_exists($this->model, $clause)
+          || class_method_exists($this->model, str_plural($clause));
+    });
+
+    $clauses = array_map_keys(function ($val) {
+      return sprintf('where%sId', ucwords($val));
+    }, $clauses);
+
+    if (count($clauses) > 0) {
+      $firstKey = array_keys($clauses)[0];
+
+      $query = call_user_func([$this->model, $firstKey], $clauses[$firstKey]);
+      array_shift($clauses);
+      foreach ($clauses as $where => $value)
+        $query->{$where}($value);
+
+      $query = $query->paginate(config('oparl.pageElements'));
+    } else
+    {
+      return $this->respondWithNotAllowed("The requested query method is not allowed on `{$this->getModelName()}`.");
+    }
+
+    return $query;
+  }
+
+  private function parseInclude($include, $query)
+  {
+    $eagerLoadingKeys = explode(',', $include);
+
+    foreach ($eagerLoadingKeys as $eagerLoadingKey)
+    {
+      if (class_method_exists($this->model, $eagerLoadingKey))
+      {
+        $query->load($eagerLoadingKey);
+        continue;
+      }
+
+      $eagerLoadingKey = str_plural($eagerLoadingKey);
+      if (class_method_exists($this->model, $eagerLoadingKey))
+        $query->load($eagerLoadingKey);
+    }
+
+    return $query;
+  }
+
   public function index()
   {
     $query = null;
 
-    if (Request::input('where'))
+    if (Request::has('where'))
     {
-      $clauses = decode_where(Request::input('where'));
+      $parsed = $this->parseWhere(Request::input('where'));
+      if ($parsed instanceof \HttpResponse)
+        return $parsed;
 
-      $clauses = array_filter_keys($clauses, function($clause) {
-        return class_method_exists($this->model, $clause)
-            || class_method_exists($this->model, str_plural($clause));
-      });
-
-      $clauses = array_map_keys(function ($val) {
-        return sprintf('where%sId', ucwords($val));
-      }, $clauses);
-
-      if (count($clauses) > 0)
-      {
-        $firstKey = array_keys($clauses)[0];
-
-        $query = call_user_func([$this->model, $firstKey], $clauses[$firstKey]);
-        array_shift($clauses);
-        foreach ($clauses as $where => $value)
-          $query->{$where}($value);
-
-        $query = $query->paginate(config('oparl.pageElements'));
-      } else
-      {
-        return $this->respondWithNotAllowed("The requested query method is not allowed on `{$this->getModelName()}`.");
-      }
+      $query = $parsed;
+    } else
+    {
+      $query = call_user_func([$this->model, 'paginate'], config('oparl.pageElements'));
     }
 
-    if (!$query) $query = call_user_func([$this->model, 'paginate'], config('oparl.pageElements'));
-
-    /*if (Request::input('include'))
-    {
-      $query->load(explode(',', Request::input('include')));
-    }*/
+    if (Request::has('include'))
+      $query = $this->parseInclude(Request::input('include'), $query);
 
     return $this->respondWithPaginated($query);
   }
