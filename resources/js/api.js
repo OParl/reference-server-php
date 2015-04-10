eb4f617175dd57f58330bbb76547e416583e6d77
Stefan Graupner <stefan.graupner@gmail.com>
2015-04-10 11:23:53 +0200

    Also removing single quotes from urls.

--- resources/js/api.js
+++ resources/js/api.js
@@ -1,6 +1,8 @@
 Prism.hooks.add('wrap', function(env) {
     if (env.type == 'string', env.content.match(/http/))
     {
-        env.content = "<a href=\"" + env.content.replace('"', '') + "\">" + env.content + "</a>";
+        var url = env.content.replace('"', '');
+        url = url.replace("'", '');
+        env.content = "<a href=\"" + url + "\">" + env.content + "</a>";
     }
 });
