c36ab069f73fa096f3579835314476224b61a6a0
Stefan Graupner <stefan.graupner@gmail.com>
2015-05-25 19:57:45 +0200

    Add mysql field loader to factory.

--- app/Services/APIQueryService/ModelFieldLoaderFactory.php
+++ app/Services/APIQueryService/ModelFieldLoaderFactory.php
@@ -7,6 +7,8 @@ class ModelFieldLoaderFactory
     switch ($databaseSystem)
     {
       case 'sqlite': return new SQLiteModelFieldLoader($table); break;
+      case 'mysql': return new MySQLModelFieldLoader($table); break;
+
       default:
         throw new \RuntimeException("{$databaseSystem} is not supported.");
     }
