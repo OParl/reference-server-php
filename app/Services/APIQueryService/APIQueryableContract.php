1acb8411c9a3e5d9fe3effe16eb8dbb0cf61ce06
Stefan Graupner <stefan.graupner@gmail.com>
2015-05-25 18:18:08 +0200

    Removed unused classes.

--- app/Services/APIQueryService/APIQueryableContract.php
+++
@@ -1,7 +0,0 @@
-<?php namespace App\Services\APIQueryService;
-
-interface APIQueryableContract
-{
-  public static function getQueryableRelations();
-  public static function getQueryableFields(array $defaultFields);
-}
