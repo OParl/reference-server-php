12cdb4902737347a62ae0894db00ecb9b2918ff3
Stefan Graupner <stefan.graupner@gmail.com>
2015-05-25 19:38:43 +0200

    Query scope for body of meeting.

--- app/Http/Controllers/API/MeetingController.php
+++ app/Http/Controllers/API/MeetingController.php
@@ -1,8 +1,19 @@
 <?php namespace App\Http\Controllers\API;
 
+use App\Services\APIQueryService\APIQueryService;
+use App\Services\APIQueryService\ValueExpression;
+
 class MeetingController extends APIController {
 	protected $model = 'OParl\Meeting';
 
   use APIIndexPaginatedTrait;
   use APIShowItemTrait;
+
+  protected function queryBody(APIQueryService &$query, ValueExpression $valueExpression)
+  {
+    $query->whereRaw(
+      \DB::raw("organization_id = (select id from organizations where body_id {$valueExpression->getExpression()} ?)"),
+      [intval($valueExpression->getValue())]
+    );
+  }
 }
