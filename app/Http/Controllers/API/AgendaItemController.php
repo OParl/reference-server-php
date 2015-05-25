67fe3cca4ea19d71d457302009a3878dd1c601c1
Stefan Graupner <stefan.graupner@gmail.com>
2015-05-25 22:56:45 +0200

    Formatting...

--- app/Http/Controllers/API/AgendaItemController.php
+++ app/Http/Controllers/API/AgendaItemController.php
@@ -1,7 +1,7 @@
 <?php namespace App\Http\Controllers\API;
 
 class AgendaItemController extends APIController {
-	protected $model = 'OParl\AgendaItem';
+  protected $model = 'OParl\AgendaItem';
 
   use APIIndexPaginatedTrait;
   use APIShowItemTrait;
