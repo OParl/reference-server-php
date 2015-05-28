4b5d639eb2d63c00df77dc3d962d68608a33d629
Stefan Graupner <stefan.graupner@gmail.com>
2015-05-28 19:21:03 +0200

    Implemented body querying.

--- app/Http/Controllers/API/MembershipController.php
+++ app/Http/Controllers/API/MembershipController.php
@@ -1,8 +1,18 @@
 <?php namespace App\Http\Controllers\API;
 
+use App\Services\APIQueryService\APIQueryService;
+use App\Services\APIQueryService\ValueExpression;
+
 class MembershipController extends APIController {
 	protected $model = 'OParl\Membership';
 
   use APIIndexPaginatedTrait;
   use APIShowItemTrait;
+
+  protected function queryBody(APIQueryService &$query, ValueExpression $valueExpression)
+  {
+    $where = "(select count(*) from people where body_id {$valueExpression->getExpression()} ? and id = memberships.person_id)";
+
+    $query->whereRaw($where, [$valueExpression->getValue()]);
+  }
 }
