e1bced1fb35d3fd30dfaf33a096c249e29113855
Stefan Graupner <stefan.graupner@gmail.com>
2015-05-28 11:18:34 +0200

    Restructuring.

--- resources/views/client/detail.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="panel panel-default">
-    <div class="panel-body">
-        <div class="center-block">
-            <div class="text-center">
-                Bitte eine Gruppierung auswählen&hellip;
-            </div>
-        </div>
-    </div>
-</div>
