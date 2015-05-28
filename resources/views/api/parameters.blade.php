7fcf60f2fd5d1b3b85eea4f735453650931e7317
Stefan Graupner <stefan.graupner@gmail.com>
2015-05-28 16:41:15 +0200

    Lot's of documentation on query parameters

--- resources/views/api/parameters.blade.php
+++ resources/views/api/parameters.blade.php
@@ -8,21 +8,77 @@
         <dl class="dl-horizontal">
             <dt>format</dt>
             <dd>
-                Der <code>format</code>-Parameter gibt das Ausgabeformat an. Weitere Informationen
-                dazu finden sich im Tab <a href="#access" data-toggle="tab" aria-controls="access">Zugriff</a>.
+                <div><strong>Datentyp:&nbsp;</strong><samp>integer</samp></div>
+
+                <p>
+                    Der <code>format</code>-Parameter gibt das Ausgabeformat an.
+                    Weitere Informationen dazu finden sich im Tab
+                    <a href="#access" data-toggle="tab" aria-controls="access">Zugriff</a>.
+                </p>
             </dd>
             <dt>where</dt>
             <dd>
+                <div><strong>Datentyp:&nbsp;</strong><samp>string</samp></div>
+                <div>
+                    <strong>Format:&nbsp;</strong><samp>(feld:wert,?)+</samp>
+                    {{--
+                        TODO: Popover with more information on API query strings
+                        <button></button>
+                    --}}
+                </div>
+
                 Der <code>where</code>-Parameter ermöglicht das Filtern der Ausgabe von Listen.
                 Er hat keine Wirkung auf die Ausgabe von einzelnen Entities.
             </dd>
+            <dt>page</dt>
+            <dd>
+                <div><strong>Datentyp:&nbsp;</strong><samp>integer</samp></div>
+                <div><strong>Wertebereich:&nbsp;</strong><samp>[1,&infin;)</samp></div>
+
+                Der <code>page</code>-Parameter ermöglicht das Wechseln zwischen den
+                verschiedenen Seiten einer Listenausgabe. Er wird ausgewertet, sobald
+                eine Abfrage {{ config('oparl.itemsPerPage') }} oder den in <code>limit</code>
+                angegebenen Wert an Elementen übersteigt.
+            </dd>
             <dt>limit</dt>
             <dd>
-                Mit dem <code>limit</code>-Parameter lässt sich die Anzahl von ausgegebenen Listenelementen
-                beschränken. Der Parameter funktioniert – ebenso wie <code>where</code> nur
-                auf Listenanfragen und hat keine Wirkung, wenn einzelne Entities abgefragt werden.
+                <div><strong>Datentyp:&nbsp;</strong><samp>integer</samp></div>
+                <div><strong>Wertebereich:&nbsp;</strong><samp>[0,&infin;)</samp></div>
 
-                <h3>Syntax</h3>
+                <p>
+                    Mit dem <code>limit</code>-Parameter lässt sich die Anzahl von ausgegebenen
+                    Listenelementen beschränken. Der Parameter funktioniert – ebenso wie
+                    <code>where</code> – nur auf Listenanfragen und hat keine Wirkung,
+                    wenn einzelne Entities abgefragt werden.
+                </p>
+
+                <p>
+                    Die Angabe von <code>limit=0</code> führt zur selben Ausgabe wie das Weglassen
+                    des Parameters. Standardmäßig werden dann maximal {{ config('oparl.itemsPerPage') }}
+                    Elemente pro Seite ausgegeben.
+                </p>
+            </dd>
+
+            <dt>only</dt>
+            <dd>
+                <p>
+                    Der <code>only</code>-Parameter ermöglicht es, die Ausgabe auf den Daten- oder
+                    den Metaabschnitt zu reduzieren. Die Angabe des Parameters ist <em>optional</em>
+                    und wird sowohl bei Listen, als auch bei Einzelelementen ausgewertet.
+                </p>
+
+                <p>
+                    Der Parameter kann folgende Werte annehmen:
+                </p>
+
+                <ul>
+                    <li><samp>data</samp>: Zeige nur Datenabschnitt</li>
+                    <li><samp>meta</samp>: Zeige nur Metaabschnitt</li>
+                    <li>
+                        <samp>data,meta</samp>: Zeige Daten- und Metaabschnitt
+                        (Äquivalent zum Weglassen des Parameters.)
+                    </li>
+                </ul>
             </dd>
         </dl>
 
