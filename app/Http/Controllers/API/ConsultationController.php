3b31e5846fb6994876969119039abf4dd8107742
Stefan Graupner <stefan.graupner@gmail.com>
2015-05-29 17:10:35 +0200

    Thoughts on body querying for consultations.

--- app/Http/Controllers/API/ConsultationController.php
+++ app/Http/Controllers/API/ConsultationController.php
@@ -1,8 +1,23 @@
 <?php namespace App\Http\Controllers\API;
 
+use App\Services\APIQueryService\APIQueryService;
+use App\Services\APIQueryService\ValueExpression;
+
 class ConsultationController extends APIController {
 	protected $model = 'OParl\Consultation';
 
   use APIIndexPaginatedTrait;
   use APIShowItemTrait;
+
+  /**
+   * Resolve Body queries
+   *
+   * @param APIQueryService $query
+   * @param ValueExpression $valueExpression
+   **/
+  public function queryBody(APIQueryService $query, ValueExpression $valueExpression)
+  {
+    // technically, consultations are linked to organizations, so that should be a way
+    // to access body_id-s
+  }
 }
