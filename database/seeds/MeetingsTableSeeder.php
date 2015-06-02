16816de3fd4277d731ca9ea9eebe6f9508d9045d
Stefan Graupner <stefan.graupner@gmail.com>
2015-06-02 11:14:57 +0200

    Slight speedup for meetings controller (not generating invitation files for now)

--- database/seeds/MeetingsTableSeeder.php
+++ database/seeds/MeetingsTableSeeder.php
@@ -20,26 +20,36 @@ class MeetingsTableSeeder extends Seeder {
 
       foreach ($body->organizations as $organization)
       {
-        foreach (range(1, static::$faker->numberBetween(150, 350)) as $meetingNumber)
+        // 3 bodies * [5,10] organizations * [150,200] meetings = [2250, 6000] meetings
+        $numberOfMeetings = static::$faker->numberBetween(150, 200);
+        for ($i = 0; $i < $numberOfMeetings; $i++)
         {
           $start = Carbon::instance(static::$faker->dateTimeBetween($termStart, $termEnd));
           $end   = Carbon::instance($start)->addHours(3);
 
           $meeting = Meeting::create([
-            'start_date'    => $start,
-            'end_date'      => (static::$faker->boolean(40)) ? $end : null,
-            'locality' => static::$faker->optional()->address,
+            'start_date' => $start,
+            'end_date'   => (static::$faker->boolean(40)) ? $end : null,
+            'locality'   => static::$faker->optional()->address,
           ]);
 
-          $meeting->organization()->associate($organization);
+          $meeting->organization_id = $organization->id;
+
           $meeting->chair()->associate($organization->members->random());
           $meeting->scribe()->associate($organization->members->random());
 
           $meeting->participants()->saveMany($this->getRandomArrayFromCollection($organization->members)->all());
           $this->getAgendaItems($meeting);
 
-          $invitationFile = static::$faker->oparlMeetingInvitation($meeting, $termStart, $termEnd);
-          $meeting->invitations()->save($invitationFile);
+          if ($meeting->agendaItems()->count() == 0)
+          {
+            // we're done here, all agenda items have been used up
+            $meeting->delete();
+            break 2;
+          }
+
+          //$invitationFile = static::$faker->oparlMeetingInvitation($meeting, $termStart, $termEnd);
+          //$meeting->invitations()->save($invitationFile);
 
           $meeting->save();
         }
@@ -49,12 +59,23 @@ class MeetingsTableSeeder extends Seeder {
 
   protected function getAgendaItems(Meeting $meeting)
   {
-    $numItems = static::$faker->numberBetween(10, 45);
+    // 75000 agenda items / 6000 meetings == 12.5 agenda items / meeting
+    $numItems = static::$faker->numberBetween(7, 14);
 
+    $items = [];
     for ($i = 0; $i < $numItems; $i++)
     {
-      $agendaItem = AgendaItem::find($this->lastAgendaItemID++);
-      $meeting->agendaItems()->save($agendaItem);
+      try
+      {
+        $agendaItem = AgendaItem::findOrFail($this->lastAgendaItemID++);
+        $items[] = $agendaItem;
+      } catch (\Exception $e)
+      {
+        \Log::error("Tried to request too many agenda items: {$this->lastAgendaItemID}.");
+      }
     }
+
+    if (count($items) > 0)
+      $meeting->agendaItems()->saveMany($items);
   }
 }
